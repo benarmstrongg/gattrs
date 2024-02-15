2c65b9162c49ce075e8b6bf3f8040407720b5d92
ben <benarmstrongg@gmail.com>
2024-02-15 14:46:19 -0500

    Add counter example.

---
+++ example/counter/src/main.rs
@@ -0,0 +1,50 @@
+use gattrs::{
+    gatt::{CharacteristicRegister, GattApplication},
+    gatt_characteristic, gatt_service,
+};
+
+#[gatt_service(uuid = "42000000-0000-0000-0000-000000000000")]
+struct CounterService {}
+
+impl CounterService {
+    fn get_characteristics(&self) -> Vec<impl CharacteristicRegister> {
+        vec![CountCharacteristic::default()]
+    }
+}
+
+#[gatt_characteristic(
+    uuid = "42000000-0000-0000-0000-000000000001",
+    flags = ["read", "write", "notify"]
+)]
+struct CountCharacteristic {
+    count: u8,
+}
+
+impl CountCharacteristic {
+    async fn read(&self) -> gattrs::GattReadResult {
+        Ok(vec![self.count])
+    }
+
+    async fn write(&mut self, val: &[u8]) -> gattrs::GattWriteResult {
+        let count = val.first().unwrap_or(&self.count).to_owned();
+        self.value = [count].to_vec();
+        self.count = count;
+        Ok(())
+    }
+}
+
+#[tokio::main]
+async fn main() -> gattrs::zbus::Result<()> {
+    GattApplication::new()
+        .await
+        .service(CounterService::default())
+        .await
+        .advertise()
+        .serve()
+        .await?;
+
+    let session_bus = zbus::Connection::session().await.unwrap();
+    GattApplication::on_bus(session_bus);
+
+    Ok(())
+}
